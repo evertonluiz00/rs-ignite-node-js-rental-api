64bfc74a296976673d1841c6fa4d7853902b0f25
Everton Christmann <evertonluiz00@hotmail.com>
2021-11-19 00:18:49 -0300

    Refactoring

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -35,6 +35,7 @@ class ImportCategoryUseCase {
                     categories.push(category);
                 })
                 .on("end", () => {
+                    fs.promises.unlink(file.path);
                     resolve(categories);
                 })
                 .on("error", (err) => {
