c11dcac2e7c3fee5287e5c872f491cd4ae50e7a5
Everton Christmann <evertonluiz00@hotmail.com>
2021-12-01 22:49:07 -0300

    Created user model

---
+++ src/modules/accounts/models/User.ts
@@ -0,0 +1,43 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity("users")
+class User {
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    driver_license: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor(name: string, username: string, email:string, password: string, driver_license: string) {
+        this.id = uuidv4();
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.driver_license = driver_license;
+        this.admin = false;
+        this.created_at = new Date();
+    }
+}
+
+export { User }
