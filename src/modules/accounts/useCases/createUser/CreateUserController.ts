49bfbc1cd4c35baf89f6aab2ca8a21d5a550f014
Everton Christmann <evertonluiz00@hotmail.com>
2021-12-03 23:53:45 -0300

    User authentication

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,6 +1,5 @@
 import { Request, Response} from 'express';
 import { container } from "tsyringe"
-import { User } from '../../models/User';
 import { CreateUserUseCase } from "./CreateUserUseCase"
 
 class CreateUserController {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import categoriesRouter from "./categories.routes";
 import specificationsRouter from "./specifications.routes";
 import usersRouter from "./users.routes";
+import authenticateRouter from "./authenticate.routes";
 
 const routes = Router();
 
 routes.use('/categories', categoriesRouter);
 routes.use('/specifications', specificationsRouter);
 routes.use('/users', usersRouter);
+routes.use(authenticateRouter);
 
 export default routes;
